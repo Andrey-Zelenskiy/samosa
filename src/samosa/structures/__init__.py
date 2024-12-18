8d31cdd11b7e9a5bd16b0fbe2606ad57972b920d
Andrey <andrey.zelenskiy@universite-paris-saclay.fr>
2024-12-18 11:58:51 +0100

    Moved Particle class to samosa/structures

--- src/samosa/structures/__init__.py
+++ src/samosa/structures/__init__.py
@@ -464,3 +464,93 @@ class Lattice:
         cls = self.__class__.__name__
         return f"{cls}(dimension={self.dimension}, "\
                f"space_group_index={self.space_group_index})"
+
+
+"""
+-------------------------------------------------------------------------------
+Particle class
+-------------------------------------------------------------------------------
+"""
+
+class Particle:
+    """
+    Defines a container for storing all relevant structural information,
+    symmetry, and interactions about a polyhedral particle. 
+    """
+
+    # Particle intializers
+    def __init__(self, 
+                 symmetry_group, 
+                 face_positions, 
+                 calculate_faces=False):
+        """
+        Constructs a particle from point group symmetry and face coordinates 
+        that match the basis of the group operators.  
+        (not necessary to have all faces)
+
+        Arguments:
+        symmetry_group - Group, point group of the particle shape;
+
+        face_positions - list, face coordinates written in the basis of the 
+                         symmetry_group.
+
+        """
+        # Type checks
+        check_type('symmetry_group', symmetry_group, Group)
+        check_type('face_positions', face_positions, list)
+
+        # Calculate the positions of the faces and store the symmetry
+        # properties
+        self.__face_groups = Orbit.sort_into_orbits(face_positions, 
+                                                    symmetry_group)
+
+        # Place patches on the particle
+
+
+    @classmethod
+    def from_lattice(cls, lattice):
+        """
+        Shortcut to initialize a Particle object from a Lattice object.
+        
+        Arguments:
+        lattice - Lattice, periodic structure, for which the nearest-neighbour
+                  connections define the shape of the particle.
+        """
+        # Type checks
+        check_type('lattice', lattice, Lattice)
+        
+        # Calculate the list of positions of the nearest-neighbours
+        face_positions = lattice.nearest_neighbours
+
+        # Calculate the point group of the unit cell
+        symmetry_group = lattice.point_group
+        
+        return cls(face_positions, symmetry_group)
+
+    @clssmethod
+    def simplex(cls, simplex_dimension):
+        """
+        Shortcut to initialize a Particle object from a simplex of some 
+        dimensionality.
+
+        Arguments:
+        simplex_dimension - int, dimension of the simplex particle.
+        """
+        # Type checks
+        check_type('simplex_dimension', simplex_dimension, int)
+
+        pass
+        
+    # Particle methods
+    
+
+    # Particle properties 
+    @property
+    def face_groups():
+        """
+        A list of independent orbits of particle faces.
+        """
+        return self.__face_groups
+
+
+    # Output summary functions
